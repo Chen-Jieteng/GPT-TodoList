f6b9bcc49734f37fd10a1363c8e3ee2089e5cb63
edwin99 <j.chen1150964115@gmail.com>
2023-04-08 23:22:34 -0400

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import TodoList from "./components/TodoList.vue";
+</script>
+
+<template>
+  <div class="container">
+    <div class="left-side">
+      <TodoList />
+    </div>
+    <div class="right-side">
+      <Suspense>
+        <RouterView />
+      </Suspense>
+    </div>
+  </div>
+</template>
+
+<style>
+.container {
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+}
+
+.left-side {
+  flex: 2;
+  background-color: black;
+  margin-right: 2rem; 
+}
+
+.right-side {
+  flex: 1;
+}
+</style>
