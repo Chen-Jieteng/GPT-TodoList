94927bd3b69093f1daf02a42796baac0478bc80e
edwin99 <j.chen1150964115@gmail.com>
2023-04-10 02:36:53 -0400

    Update DailySchedule.vue

--- src/components/DailySchedule.vue
+++ src/components/DailySchedule.vue
@@ -2,14 +2,36 @@
 import { inject } from "vue";
 
 const tasks = inject("tasks");
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+}
 </script>
 
 <template>
   <div class="daily-schedule">
     <strong>今日规划</strong>
     <ul>
-      <li v-for="(task, index) in tasks" :key="index">{{ task }}</li>
+      <li v-for="(task, index) in tasks" :key="index" class="task">
+        {{ task }}
+        <button class="delete-btn" @click="deleteTask(index)">Delete</button>
+      </li>
     </ul>
   </div>
 </template>
 
+<style scoped>
+.task {
+  position: relative;
+}
+
+.delete-btn {
+  position: absolute;
+  right: 0;
+  display: none;
+}
+
+.task:hover .delete-btn {
+  display: inline;
+}
+</style>
