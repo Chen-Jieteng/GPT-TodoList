3038ac1aaf4103ec7872d9a68a64df2b49d69ff3
edwin99 <j.chen1150964115@gmail.com>
2023-04-10 12:13:12 -0400

    Update AddTask.vue

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,23 +1,55 @@
 <script setup lang="ts">
-import { inject } from "vue";
+import { inject, ref, defineExpose } from "vue";
 
 const tasks = inject("tasks");
+const newTask = ref("");
 
 const addTask = () => {
-  const input = document.getElementById("task-input");
-  if (input.value.trim()) {
-    tasks.value.push(input.value.trim());
-    input.value = "";
+  const parsedInput = parseInput(newTask.value);
+  if (!parsedInput) {
+    window.alert("输入的任务格式错误，请重新输入");
+    return;
+  }
+
+  const { duration, taskName } = parsedInput;
+  if (duration <= 2) {
+    window.alert("任务时长需至少2分钟");
+    return;
+  }
+
+  if (/^\d+$/.test(taskName)) {
+    window.alert("任务名字不能只包含数字");
+    return;
+  }
+
+  if (taskName) {
+    tasks.value.push({
+      content: taskName,
+      duration,
+    });
+    newTask.value = "";
   }
 };
+
+function parseInput(input) {
+  const match = input.match(/(\d+)(分钟|min)[^\d]*(.*)/i);
+  if (match) {
+    const duration = parseInt(match[1]);
+    const taskName = match[3].trim();
+    return { duration, taskName };
+  }
+  return null;
+}
+
+defineExpose({
+  addTask,
+  newTask,
+});
 </script>
 
 <template>
   <div class="add-task">
-    <input id="task-input" type="text" placeholder="添加新任务" />
-    <button @click="addTask">添加</button>
+    <input id="task-input" type="text" placeholder="Add new task" v-model="newTask" />
+    <button @click="addTask">添加任务</button>
   </div>
 </template>
-
-
-
