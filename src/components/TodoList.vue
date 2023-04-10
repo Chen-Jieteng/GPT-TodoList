7a2b1c72e200ca5eac5a4ab69fda790a640eae2a
edwin99 <j.chen1150964115@gmail.com>
2023-04-10 02:27:59 -0400

    Update TodoList.vue

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,85 +1,61 @@
+<script setup lang="ts">
+
+import { ref, provide } from "vue";
+
+import Day from "./Day.vue";
+import AddTask from "./AddTask.vue";
+import DailySchedule from "./DailySchedule.vue";
+import CompletedTaskList from "./CompletedTaskList.vue";
+import IncompletedTaskList from "./IncompletedTaskList.vue";
+import ReasonIncomplete from "./ReasonIncomplete.vue";
+import DailyResult from "./DailyResult.vue";
+import DailyThought from "./DailyThought.vue";
+import DailyRevoke from "./DailyRevoke.vue";
+import DailyImprovement from "./DailyImprovement.vue";
+
+const tasks = ref([]);
+provide("tasks", tasks);
+
+</script>
+
 <template>
   <div class="todo-container">
     <div class="layer layer-1">
-      <div class="day-title day-1"><strong>Day 1</strong></div>
-      <div class="add-task">
-        <!-- Add new task input or form goes here -->
-      </div>
+      <Day />
+      <AddTask />
     </div>
     <div class="layer layer-2">
-      <strong>今日规划</strong>
+      <DailySchedule />
     </div>
     <div class="layer layer-3">
-      <div class="completed-tasks">
-        <strong>已完成任务</strong>
-      </div>
-      <div class="incompleted-tasks">
-        <strong>什么没完成？</strong>
-      </div>
-      <div class="incompletion-reasons">
-        <strong>为什么没完成？</strong>
-      </div>
+      <CompletedTaskList />
+      <IncompletedTaskList />
+      <ReasonIncomplete />
     </div>
     <div class="layer layer-4">
-      <div class="learnings">
-        <strong>今天有哪些收获？</strong>
-      </div>
-      <div class="ideas">
-        <strong>今天有什么想法？</strong>
-      </div>
-      <div class="inspiring-quotes">
-        <strong>引发思考的几句话</strong>
-      </div>
+      <DailyResult />
+      <DailyThought />
+      <DailyRevoke />
     </div>
     <div class="layer layer-5">
-      <!-- List of descriptions for improvement goes here -->
+      <DailyImprovement />
     </div>
   </div>
 </template>
+
 <style scoped>
 .todo-container {
   background-color: #f5f5f5;
   padding: 1rem;
-  margin-left: 2rem;
-  width: 100%;
-  height: 80%;
+  margin: 2rem;
+  width: 90%;
+  height: 90%;
   overflow-y: auto;
   border-radius: 5px;
 }
 
 .layer {
-  margin-bottom: 1rem;
-}
-
-.layer-1 {
-  display: flex;
-  width: 100%;
-}
-
-.day-title {
-  width: 20%;
-}
-
-.day-1 {
-  color: red; /* apply red color to Day 1 */
-}
-
-.add-task {
-  width: 80%;
-}
-
-.layer-3,
-.layer-4 {
-  display: flex;
-  justify-content: space-between;
-}
-
-.completed-tasks,
-.incompleted-tasks,
-.incompletion-reasons,
-.learnings,
-.ideas,
-.inspiring-quotes {
-  width: 32%;
+  margin-bottom: 1.5rem;
 }
 </style>
+
