fa4a2bc26a2000eab682d495c903342d74efd659
edwin99 <j.chen1150964115@gmail.com>
2023-04-10 02:27:15 -0400

    Create DailySchedule.vue

---
+++ src/components/DailySchedule.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { inject } from "vue";
+
+const tasks = inject("tasks");
+</script>
+
+<template>
+  <div class="daily-schedule">
+    <strong>今日规划</strong>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">{{ task }}</li>
+    </ul>
+  </div>
+</template>
+
