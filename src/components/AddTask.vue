188eecca40276570ed282dc9a3f7c26892da5051
edwin99 <j.chen1150964115@gmail.com>
2023-04-10 02:26:20 -0400

    Create AddTask.vue

---
+++ src/components/AddTask.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { inject } from "vue";
+
+const tasks = inject("tasks");
+
+const addTask = () => {
+  const input = document.getElementById("task-input");
+  if (input.value.trim()) {
+    tasks.value.push(input.value.trim());
+    input.value = "";
+  }
+};
+</script>
+
+<template>
+  <div class="add-task">
+    <input id="task-input" type="text" placeholder="Add new task" />
+    <button @click="addTask">Add Task</button>
+  </div>
+</template>
+
+
+
