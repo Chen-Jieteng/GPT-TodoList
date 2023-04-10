b338c9a85f6e7b824eed8540aca155d28ae65ce3
edwin99 <j.chen1150964115@gmail.com>
2023-04-10 02:53:19 -0400

    Create CompletedTaskList.vue

---
+++ src/components/CompletedTaskList.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { inject } from "vue";
+
+const completedTasks = inject("completedTasks");
+</script>
+
+<template>
+  <div class="completed-tasks">
+    <strong>已完成任务</strong>
+    <ul>
+      <li v-for="(task, index) in completedTasks" :key="index">
+        {{ task }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+/* style */
+</style>
