36554b81b9a91368ace67150ec57443e695a4b59
edwin99 <j.chen1150964115@gmail.com>
2023-04-11 04:46:38 -0400

    Create chatgpt.ts

---
+++ src/lib/chatgpt.ts
@@ -0,0 +1,30 @@
+import type { ChatMessage } from "@/types";
+
+const chat = async (messageList: ChatMessage[]) => {
+  const request = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer xxxx`, // api
+    },
+    body: JSON.stringify({
+      stream: true,
+      messages: messageList,
+      model: "gpt-3.5-turbo",
+    }),
+  };
+
+  try {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", request);
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+};
+
+export { chat };
