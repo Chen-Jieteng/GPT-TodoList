dfdbab44d79f0b5cdee95590531dcbb88eff7a93
edwin99 <j.chen1150964115@gmail.com>
2023-04-11 00:13:48 -0400

    New requirement: Auto Task Parser
    Auto Parser for single task: 
    Reasons to add such requirement: I'm lazy to set timer myself, just wanna make things easy and fast. 
    
    steps:
    1. e.g. task added: "Finishing homeworks in 20 minutes"
    2. current time: 12:02PM, duration: 20min, ending time: 12:22PM
    3. the starting time of next task starts with the one of previous task. 
    4. etc.

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,8 +1,60 @@
 <script setup lang="ts">
-import { inject, ref, defineExpose } from "vue";
+import { ref } from "vue";
+import dayjs from "dayjs";
 
-const tasks = inject("tasks");
 const newTask = ref("");
+
+interface Task {
+  id: number;
+  content: string;
+  duration: number;
+  startingTime: string;
+}
+
+const props = defineProps<{ tasks: Task[] }>();
+const tasks = props.tasks;
+
+const parseInput = (input: string): { duration: number; taskName: string } | null => {
+  const regex = /^(\d+)分钟(.+)$/;
+  const match = input.match(regex);
+  if (!match) return null;
+
+  return {
+    duration: parseInt(match[1], 10),
+    taskName: match[2].trim(),
+  };
+};
+
+/* const addTask = () => {
+  const parsedInput = parseInput(newTask.value);
+  if (!parsedInput) {
+    window.alert("输入的任务格式错误，请重新输入");
+    return;
+  }
+
+  const { duration, taskName } = parsedInput;
+
+  if (duration < 5) {
+    window.alert("任务时长需至少5分钟");
+    return;
+  }
+
+  if (!isNaN(Number(taskName))) {
+    window.alert("任务名字不能仅包含数字");
+    return;
+  }
+
+  if (taskName) {
+    const taskId = Date.now();
+    tasks.push({
+      id: taskId,
+      content: taskName,
+      duration,
+      startingTime: dayjs().format("HH:mm A"),
+    });
+    newTask.value = "";
+  }
+}; */
 
 const addTask = () => {
   const parsedInput = parseInput(newTask.value);
@@ -12,44 +64,43 @@ const addTask = () => {
   }
 
   const { duration, taskName } = parsedInput;
-  if (duration <= 2) {
-    window.alert("任务时长需至少2分钟");
+
+  if (duration < 5) {
+    window.alert("任务时长需至少5分钟");
     return;
   }
 
-  if (/^\d+$/.test(taskName)) {
-    window.alert("任务名字不能只包含数字");
+  if (!isNaN(Number(taskName))) {
+    window.alert("任务名字不能仅包含数字");
     return;
   }
 
   if (taskName) {
-    tasks.value.push({
+    const taskId = Date.now();
+    const startingTime = dayjs().format("HH:mm A");
+    const endingTime = dayjs()
+      .add(duration, "minute")
+      .format("hh:mm A");
+
+    window.alert(
+      `Starting Time: ${startingTime}\nEnding Time: ${endingTime}\nDuration: ${duration}min`
+    );
+
+    tasks.push({
+      id: taskId,
       content: taskName,
       duration,
+      startingTime,
     });
     newTask.value = "";
   }
 };
 
-function parseInput(input) {
-  const match = input.match(/(\d+)(分钟|min)[^\d]*(.*)/i);
-  if (match) {
-    const duration = parseInt(match[1]);
-    const taskName = match[3].trim();
-    return { duration, taskName };
-  }
-  return null;
-}
-
-defineExpose({
-  addTask,
-  newTask,
-});
 </script>
 
 <template>
   <div class="add-task">
-    <input id="task-input" type="text" placeholder="Add new task" v-model="newTask" />
+    <input id="task-input" v-model="newTask" type="text" placeholder="例如：10分钟完成英语作业" />
     <button @click="addTask">添加任务</button>
   </div>
 </template>
