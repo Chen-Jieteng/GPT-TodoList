f2222cc4bc5fed6a50c9869363306d282dbc16ec
edwin99 <j.chen1150964115@gmail.com>
2023-04-14 23:53:54 -0400

    Update DailyImprovement.vue

--- src/components/DailyImprovement.vue
+++ src/components/DailyImprovement.vue
@@ -8,3 +8,17 @@
         <li>example</li>
     </div>
 </template>
+
+<style> 
+    .daily-improvement {
+      background-color:darkseagreen;
+      padding: 1rem;
+      border-radius: 5px;
+      transition: transform 0.3s ease, box-shadow 0.3s ease;
+    }
+
+    .daily-improvement:hover {
+      transform: translateY(-2px) scale(1.0);
+      box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.1);
+    }
+</style>
