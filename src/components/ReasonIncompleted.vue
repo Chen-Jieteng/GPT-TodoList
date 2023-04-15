8428f4903d5f9d66d63f4516866ac113543fa154
edwin99 <j.chen1150964115@gmail.com>
2023-04-14 23:56:19 -0400

    Update ReasonIncompleted.vue

--- src/components/ReasonIncompleted.vue
+++ src/components/ReasonIncompleted.vue
@@ -4,3 +4,17 @@
         <li>无数据</li>
     </div>
 </template>
+
+<style> 
+    .reason-incompleted {
+      background-color:darkseagreen;
+      padding: 1rem;
+      border-radius: 5px;
+      transition: transform 0.3s ease, box-shadow 0.3s ease;
+    }
+
+    .reason-incompleted :hover {
+      transform: translateY(-2px) scale(1.0);
+      box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.1);
+    }
+</style>
