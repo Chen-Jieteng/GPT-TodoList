27aae22334abbed1a2db368606dc10705f9fe247
edwin99 <j.chen1150964115@gmail.com>
2023-04-14 23:55:13 -0400

    Update DailyThought.vue

--- src/components/DailyThought.vue
+++ src/components/DailyThought.vue
@@ -1,3 +1,17 @@
 <template>
     <strong>今天有什么想法？</strong>
 </template>
+<style> 
+    .daily-thought {
+      background-color:darkseagreen;
+      padding: 1rem;
+      border-radius: 5px;
+      transition: transform 0.3s ease, box-shadow 0.3s ease;
+    }
+
+    .daily-thought:hover {
+      transform: translateY(-2px) scale(1.0);
+      box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.1);
+    }
+</style>
+
