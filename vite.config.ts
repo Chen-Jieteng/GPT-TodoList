3f2e47dfee3b7132bd29ede71cde31ffb9b5ff5c
edwin99 <j.chen1150964115@gmail.com>
2023-04-14 23:59:10 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,14 @@
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+import { URL, fileURLToPath } from "node:url";
+
+export default defineConfig({
+  plugins: [vue()],
+  base: "./",
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
 
